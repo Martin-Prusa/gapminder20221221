1036f0ac4749eeaac382df0e3951f2bae2e78f9f
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-12-22 10:58:23 +0100

    TWR 1

--- src/main/java/cz/martin/repositories/GapminderRepository.java
+++ src/main/java/cz/martin/repositories/GapminderRepository.java
@@ -14,21 +14,19 @@ public class GapminderRepository {
 
     public List<Integer> getYears() {
         List<Integer> years = new ArrayList<>();
-        try {
-            Connection connection = DriverManager.getConnection(this.databaseURL);
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("""
-                SELECT DISTINCT G.year
-                FROM gapminder AS G
-                ORDER BY G.year
-            """);
+        try (
+                Connection connection = DriverManager.getConnection(this.databaseURL);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("""
+                            SELECT DISTINCT G.year
+                            FROM gapminder AS G
+                            ORDER BY G.year
+                        """)
+        ) {
 
             while (resultSet.next()) {
                 years.add(resultSet.getInt(1));
             }
-
-            connection.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -42,17 +40,17 @@ public class GapminderRepository {
         try {
             Connection connection = DriverManager.getConnection(databaseURL);
             PreparedStatement statement = connection.prepareStatement("""
-                SELECT G.country, G.lifeExp
-                FROM gapminder AS G
-                WHERE G.year = ?
-                ORDER BY G.country
-            """);
+                        SELECT G.country, G.lifeExp
+                        FROM gapminder AS G
+                        WHERE G.year = ?
+                        ORDER BY G.country
+                    """);
 
             statement.setInt(1, year);
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
-                countries.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2)*1000) / 1000.0));
+                countries.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2) * 1000) / 1000.0));
             }
 
             connection.close();
@@ -65,23 +63,21 @@ public class GapminderRepository {
 
     public List<Country> getContinents() {
         List<Country> continents = new ArrayList<>();
+        try (
+                Connection connection = DriverManager.getConnection(this.databaseURL);
+                Statement statement = connection.createStatement();
 
-        try {
-            Connection connection = DriverManager.getConnection(this.databaseURL);
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("""
-                SELECT G.continent, AVG(G.lifeExp)
-                FROM gapminder AS G
-                GROUP BY G.continent
-                ORDER BY G.continent
-            """);
+                ResultSet resultSet = statement.executeQuery("""
+                            SELECT G.continent, AVG(G.lifeExp)
+                            FROM gapminder AS G
+                            GROUP BY G.continent
+                            ORDER BY G.continent
+                        """)
+        ) {
 
             while (resultSet.next()) {
-                continents.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2)*1000) / 1000.0));
+                continents.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2) * 1000) / 1000.0));
             }
-
-            connection.close();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -96,19 +92,19 @@ public class GapminderRepository {
         try {
             Connection connection = DriverManager.getConnection(databaseURL);
             PreparedStatement statement = connection.prepareStatement("""
-                SELECT G.continent, AVG(G.lifeExp)
-                FROM gapminder AS G
-                WHERE G.year = ?
-                GROUP BY G.continent
-                ORDER BY G.continent
-            """);
+                        SELECT G.continent, AVG(G.lifeExp)
+                        FROM gapminder AS G
+                        WHERE G.year = ?
+                        GROUP BY G.continent
+                        ORDER BY G.continent
+                    """);
 
             statement.setInt(1, year);
 
             ResultSet resultSet = statement.executeQuery();
 
             while (resultSet.next()) {
-                continents.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2)*1000) / 1000.0));
+                continents.add(new Country(resultSet.getString(1), Math.round(resultSet.getDouble(2) * 1000) / 1000.0));
             }
 
             connection.close();
